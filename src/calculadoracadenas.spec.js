e3963a2530e678c847c5f8233d26e9f17bb446b2
Israel Antezana Rojas <israelantezana@gmail.com>
2025-11-06 15:11:30 -0400

    feat: calcula para una cadena vacia

---
+++ src/calculadoracadenas.spec.js
@@ -0,0 +1,11 @@
+//import sumar from "./sumador.js";
+
+describe("Calculadora de cadenas", () => {
+  it("deberia retornar 0 para una cadena vacia", () => {
+    expect(sumarCadena("")).toEqual(0);
+  });
+});
+
+function sumarCadena(cadena) {
+  return 0;
+}
